dc21f673d56655ccf214d3276a8a3a7460b7d539
Ben Mitchinson <mitchinson.dev@gmail.com>
2022-06-02 12:15:38 -0600

    promise chain overview + issue configuration data structure

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
-import returnBen from './returnBen';
-import NumberGen from './NumberGen';
-import { HELLO } from './configuration';
+import { issues, defaultIssueBody } from './issues_to_create.json';
 
-const gen = new NumberGen();
-const val = gen.getNum();
-console.log(returnBen());
-console.log('getNum Value:', val);
-console.log('config Value:', HELLO);
+const createGHIssue = async (issue: typeof issues[0]) => {
+    console.log('📝 Creating issue:', issue.title);
+};
+
+const assignZHEpic = async () => {
+    Promise.resolve();
+};
+
+// import json file
+issues.forEach((issue) => {
+    createGHIssue(issue)
+        .then(assignZHEpic)
+        .then(() => {
+            console.log(`✅ ${issue.title} Successfully Created`);
+        })
+        .catch((e) => {
+            console.error('❌ Error creating issue:', e);
+        });
+});
